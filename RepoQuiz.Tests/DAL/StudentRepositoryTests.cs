2fed8f65d9dc5c71e2c72e018719d162e592d2ee
Sandy Jones <sandynashv@gmail.com>
2016-10-14 21:33:48 -0500

    repo layout

--- RepoQuiz.Tests/DAL/StudentRepositoryTests.cs
+++ RepoQuiz.Tests/DAL/StudentRepositoryTests.cs
@@ -1,10 +1,155 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepoQuiz.DAL;
+using RepoQuiz.Models;
+using System.Linq;
+using System.Data.Entity;
+using Moq;
 
 namespace RepoQuiz.Tests.DAL
 {
     [TestClass]
     public class StudentRepositoryTests
     {
+        private readonly object entity;
+
+        Mock<StudentContext> mock_context { get; set; }
+        Mock<DbSet<Student>> mock_student_table { get; set; }
+        List<Student> student_list { get; set; }
+        StudentRepository repo { get; set; }
+
+        public void ConnectMockstoData()
+        {
+            var queryable_list = student_list.AsQueryable();
+            // Lie to LINQ make it think that our new Queryable List is a Database table.
+            mock_student_table.As<IQueryable<Student>>().Setup(m => m.Provider).Returns(queryable_list.Provider);
+            mock_student_table.As<IQueryable<Student>>().Setup(m => m.Expression).Returns(queryable_list.Expression);
+            mock_student_table.As<IQueryable<Student>>().Setup(m => m.ElementType).Returns(queryable_list.ElementType);
+            mock_student_table.As<IQueryable<Student>>().Setup(m => m.GetEnumerator()).Returns(() => queryable_list.GetEnumerator());
+
+            // Student property return our Queryable List AKA Fake database table.
+            mock_context.Setup(c => c.Students).Returns(mock_student_table.Object);
+
+            mock_student_table.Setup(t => t.Add(It.IsAny<Student>())).Callback((Student a) => student_list.Add(a));
+            mock_student_table.Setup(t => t.Remove(It.IsAny<Student>())).Callback((Student a) => student_list.Remove(a));
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            // Create Mock StudentContext
+            mock_context = new Mock<StudentContext>();
+            mock_student_table = new Mock<DbSet<Student>>();
+            student_list = new List<Student>(); // Fake
+            repo = new StudentRepository(mock_context.Object);
+            ConnectMockstoData();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            repo = null; // 
+        }
+
+        [TestMethod]
+        public void RepoEnsureCanCreateInstance()
+        {
+            StudentRepository repo = new StudentRepository();
+            Assert.IsNotNull(repo);
+        }
+
+        [TestMethod]
+        public void RepoEnsureRepoHasContext()
+        {
+            StudentRepository repo = new StudentRepository();
+
+            StudentContext actual_context = repo.Context;
+
+            Assert.IsInstanceOfType(actual_context, typeof(StudentContext));
+        }
+
+        [TestMethod]
+        public void RepoEnsureWeHaveNoStudents()
+        {
+            // Arrange
+
+            // Act
+            List<Student> actual_students = repo.GetStudents();
+            int expected_students_count = 0;
+            int actual_students_count = actual_students.Count;
+
+            // Assert
+            Assert.AreEqual(expected_students_count, actual_students_count);
+        }
+
+        [TestMethod]
+        public void RepoEnsureAddStudentToDatabase()
+        {
+            // Arrange
+            Student my_student = new Student { FirstName = "Jean", LastName = "Franks", StudentID = 2, Major = "Latin"  }; // Property Initializer
+           
+            // Act
+            repo.AddStudent(my_student);
+
+            // repo.AddStudent("Jean", "Franks", 2, "Latin")
+            int actual_student_count = repo.GetStudents().Count;
+            int expected_student_count = 1;
+
+            // Assert
+            Assert.AreEqual(expected_student_count, actual_student_count);
+        }
+
+        [TestMethod]
+        public void RepoEnsureAddStudentWithArgs()
+        {
+            // Arrange & Act
+            repo.AddStudent1("Jean", "Franks", 2, "Latin");
+
+            // Assert
+            List<Student> actual_students = repo.GetStudents();
+            int actual_student_studentId = actual_students.First().StudentID;
+            int expected_student_studentId = 2;
+
+            Assert.AreEqual(expected_student_studentId, actual_student_studentId);
+        }
+
+        [TestMethod]
+        public void RepoEnsureFindStudentByStudentID()
+        {
+            // Arrange
+            student_list.Add(new Student { StudentID = 2, FirstName = "Jean", LastName = "Franks", Major = "Latin" });
+            student_list.Add(new Student { StudentID = 3, FirstName = "John", LastName = "Smith", Major = "Art" });
+            student_list.Add(new Student { StudentID = 4, FirstName = "Carrie", LastName = "Ann", Major = "Music" });
+
+            // Act
+            int studentID = 3;
+            Student actual_student = repo.FindStudentByStudentID(studentID);
+
+            // Assert
+            int expected_student_id = 3;
+            int actual_student_id = actual_student.StudentID;
+            Assert.AreEqual(expected_student_id, actual_student_id);
+        }
+
+        [TestMethod]
+        public void RepoEnsureICanRemoveStudent()
+        {
+            // Arrange
+            student_list.Add(new Student { StudentID = 2, FirstName = "Jean", LastName = "Franks", Major = "Latin" });
+            student_list.Add(new Student { StudentID = 3, FirstName = "John", LastName = "Smith", Major = "Art" });
+            student_list.Add(new Student { StudentID = 4, FirstName = "Carrie", LastName = "Ann", Major = "Music" });
+
+            // Act
+            string major = "Latin";
+            Student removed_student = repo.RemoveStudent(major);
+            int expected_student_count = 2;
+            int actual_student_count = repo.GetStudents().Count;
+            int expected_student_id = 2;
+            int actual_student_id = removed_student.StudentID;
+
+            // Assert
+            Assert.AreEqual(expected_student_count, actual_student_count);
+            Assert.AreEqual(expected_student_id, actual_student_id);
+        }
     }
 }
---
+++ RepoQuiz/DAL/StudentRepository.cs
@@ -0,0 +1,80 @@
+﻿using RepoQuiz.Models;
+using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RepoQuiz.DAL
+{
+    public class StudentRepository
+    {
+        private string last_name;
+        private string _major;
+        private Student student_;
+        private int studentID;
+        private Student major;
+
+        public StudentContext Context { get; set; }
+        public string first_name { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Major { get; private set; }
+        public int StudentID { get; private set; }
+
+        public StudentRepository()
+        {
+            Context = new StudentContext();
+        }
+        public StudentRepository(StudentContext _context)
+        {
+            Context = _context;
+        }
+        public List<Student> GetStudents()
+        {
+            return Context.Students.ToList();
+        }
+        //public void AddStudent(StudentContext student_)
+        //{
+        //   // Student student_ = new Student { FirstName = first_name, LastName = last_name, StudentID = studentID, Major = _major };
+        //    Context.Students.Add(this.student_);
+        //    Context.SaveChanges();
+        //}
+        //public void AddStudent(string FirstName, string LastName)
+        //{         
+        //    Context.Students.Add(student_);
+        //    Context.SaveChanges();
+        //}
+
+        public Student FindStudentByStudentID(int studentID)
+        {
+            Student found_student = Context.Students.FirstOrDefault(a => a.StudentID == studentID);
+            return found_student;
+        }
+
+        //public void AddStudent(Student my_student)
+        //{
+        //    Context.Students.Add(my_student);
+        //    Context.SaveChanges();
+        //}
+
+
+        //public void AddStudent(StudentContext student_)
+        //{
+        //    // Student student_ = new Student { FirstName = first_name, LastName = last_name, StudentID = studentID, Major = _major };
+        //    Context.Students.Add(this.student_);
+        //    Context.SaveChanges();
+        //}
+
+        public void RemoveStudent(StudentContext major)
+        {
+
+            Context.Students.Remove(this.major);
+            Context.SaveChanges();
+        }
+
+        //public void AddStudent1(string v1, string v2, int v3, string v4)
+        //{
+        //    Context.Students.Add(FirstName = first_name, LastName = last_name, StudentID = studentID, Major = _major);
+        //}
+    }
+}
